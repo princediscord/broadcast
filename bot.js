895f4962b23cb5a0b49cd25c22b197d7c4a9887a
princediscord <51452946+princediscord@users.noreply.github.com>
2019-06-06 17:59:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,11 +38,11 @@ client.on('message', message => {
     let command = messageArray[0];
     
     
-    if (command === `${prefix}help`) {
+    if (command === `${prefix}helip`) {
       message.channel.send(`**تم الارسال في الخاص**`);
 			let embed = new Discord.RichEmbed()
                 .setColor('RANDOM')
-                .addField('     **=-=-:: [ PANDA SERVER ] ::-=-=** ' ,'╔[❖═════════════════════❖]╗')
+                .addField('     **=-=-:: [ InFinty SERVER ] ::-=-=** ' ,'╔[❖═════════════════════❖]╗')
 				.addField(`**${prefix}bc | رسالة جماعية لجميع الاعضاء**` ,'**=-=-=-=-=-=-=-=-=-=-=**')
 				.addField(`**${prefix}bc1 | رسالة جماعية للاونلاين فقط**` ,'**=-=-=-=-=-=-=-=-=-=-=**')
 				.addField(`**${prefix}bc2 | رسالة جماعية للوفلاين فقط**` ,'**=-=-=-=-=-=-=-=-=-=-=**')
@@ -145,167 +145,11 @@ client.on('message' , message => {//bcrole
 
 
 
-client.on("guildMemberAdd", member => {
-let welcomer = member.guild.channels.find('name', `🙌welcome🙌`)
-let memberavatar = member.user.avatarURL
-if (!welcomer) return;
-if(welcomer) {
-moment.locale('ar-ly');
-var mrx = member.user;
-let mrxembed = new Discord.RichEmbed()
-.setTitle(mrx.username,`#${mrx.discriminator}`)
-.addField('» Joined Discord ago',`${moment}(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-.addField('» Your Number',`${member.guild.memberCount}`,true)
-.setThumbnail(mrx.avatarURL)
-.setFooter("Zombie Games.")
-welcomer.send({embed:mrxembed});          
-}})
+
  
-client.on('guildMemberAdd', member => {
-member.guild.fetchInvites().then(guildInvites => {
-const ei = invites[member.guild.id];
-invites[member.guild.id] = guildInvites;
-const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-const inviter = client.users.get(invite.inviter.id);
-const logChannel = member.guild.channels.find(channel => channel.name === `🙌welcome🙌`);
-if(!logChannel) return;
-setTimeout(() => {
-logChannel.send(`Invited By: <@${inviter.id}>`);
-},2000)
-});
-});
- 
-client.on('guildMemberAdd',async member => {
-const Canvas = require('canvas');
-const jimp = require('jimp');
-const w = ['./welcome_4.png'];
-let Image = Canvas.Image,
-    canvas = new Canvas(800, 300),
-    ctx = canvas.getContext('2d');
-ctx.patternQuality = 'bilinear';
-ctx.filter = 'bilinear';
-ctx.antialias = 'subpixel';
-ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-ctx.shadowOffsetY = 2;
-ctx.shadowBlur = 2;
-ctx.stroke();
-ctx.beginPath();
- 
-fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-    if (err) return console.log(err);
-    let BG = Canvas.Image;
-    let ground = new Image;
-    ground.src = Background;
-    ctx.drawImage(ground, 0, 0, 800, 300);
- 
-})
- 
-        let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-        jimp.read(url, (err, ava) => {
-            if (err) return console.log(err);
-            ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-         if (err) return console.log(err);
- 
-  ctx.font = '36px Arial';
-  ctx.fontSize = '72px';
-  ctx.fillStyle = "#ffffff";
-  ctx.textAlign = "center";
-  ctx.fillText(member.user.username, 545, 177);
- 
-  ctx.font = '16px Arial Bold';
-  ctx.fontSize = '72px';
-  ctx.fillStyle = "#ffffff";
-  ctx.textAlign = "center";
-  ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
- 
-  let Avatar = Canvas.Image;
-  let ava = new Avatar;
-  ava.src = buf;
-  ctx.beginPath();
-  ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
-  ctx.closePath();
-  ctx.clip();
-  ctx.drawImage(ava, 36, 21, 260, 260);
-   
-  let c = member.guild.channels.find('name', `🙌welcome🙌`)
-  if(!c) return;
-  c.sendFile(canvas.toBuffer());
- 
-});
-});
-});
- 
-const invites = {};
- 
-const wait = require('util').promisify(setTimeout);
- 
-client.on('ready', () => {
-wait(1000);
- 
-client.guilds.forEach(g => {
-g.fetchInvites().then(guildInvites => {
-invites[g.id] = guildInvites;
-});
-});
-});
 
 
 
-
-
-
-
-var config = {
-  events: [
-    {type: "CHANNEL_CREATE", logType: "CHANNEL_CREATE", limit: 3 , delay: 3000},
-    {type: "CHANNEL_DELETE", logType: "CHANNEL_DELETE", limit: 2, delay: 3000},
-    {type: "GUILD_MEMBER_REMOVE", logType: "MEMBER_KICK", limit: 3, delay: 3000},
-    {type: "GUILD_BAN_ADD", logType: "MEMBER_BAN_ADD", limit: 3, delay: 3000}
-  ]
-}
-client.on("raw", (packet)=> {
-  let {t, d} = packet, type = t, {guild_id} = data = d || {};
-  if (type === "READY") {
-    client.startedTimestamp = new Date().getTime();
-    client.captures = [];
-  }
-  let event = config.events.find(anEvent => anEvent.type === type);
-  if (!event) return;
-  let guild = client.guilds.get(guild_id);
-  if (!guild) return;
-  guild.fetchAuditLogs({limit : 1, type: event.logType})
-    .then(eventAudit => {
-      let eventLog = eventAudit.entries.first();
-      if (!eventLog) return;
-      let executor = eventLog.executor;
-      guild.fetchAuditLogs({type: event.logType, user: executor})
-        .then((userAudit, index) => {
-          let uses = 0;
-          userAudit.entries.map(entry => {
-            if (entry.createdTimestamp > client.startedTimestamp && !client.captures.includes(entry.id)) uses += 1;
-          });
-          setTimeout(() => {
-            client.captures.push(index);
-          }, event.delay || 2000)
-          if (uses >= event.limit) {
-            client.emit("reachLimit", {
-              user: userAudit.entries.first().executor,
-              member: guild.members.get(executor.id),
-              guild: guild,
-              type: event.type,
-            })
-          }
-        }).catch(console.error)
-    }).catch(console.error)
-});
-client.on("reachLimit", (limit)=> {
-  let log = limit.guild.channels.find( channel => channel.name === "log");
-  log.send(limit.user.username+"\ try to hack !! @everyone !!");
-  limit.guild.owner.send(limit.user.username+"\ حاول التهكير الحقق (!)")
-  limit.member.roles.map(role => {
-    limit.member.removeRole(role.id)
-    .catch(log.send)
-  });
 });
 
 
