defb471cc6b2d675e948cad14a159cee1d48bc8e
princediscord <51452946+princediscord@users.noreply.github.com>
2019-06-06 18:00:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,47 +150,6 @@ client.on('message' , message => {//bcrole
 
 
 
-});
-
-
-
-
-
-
-
-const gamestats = [`$bc`,`- Destu.`,`by | NoHaxJustAbdou`,`- Destu.`]
-var index = 0
-var timer = 10 // الوقت بالثواني لتغير الستريمنق
-client.on("ready", ()=> {
-        setInterval(function(){
-        client.user.setGame(`${gamestats[index]}`,'https://www.twitch.tv/ACMBOT') 
-        index++
-            if( index >= gamestats.length) index = 0 ;
-        }, timer*1000);
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
